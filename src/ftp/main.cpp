97b3e0e8141cd89ef0b1ff543b1c055af5ed67a4
janumaruku <jean-marc.kottin@ade-labs.com>
2026-02-26 08:05:49 +0100

    feat: add test config to cmake and clean repo

---
+++ src/ftp/main.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** MyFTP
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    const int iii = 0;
+
+    printf("Hello World! %d\n", iii);
+
+    return 0;
+}
