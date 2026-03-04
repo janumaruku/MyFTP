15e64c4b10cdb329bf1039473d77b8e42660be81
janumaruku <janumaruku@users.noreply.github.com>
2026-03-04 13:51:32 +0000

    deploy: 040d26b29ddd44035e7f27928d67ae93d7802d47

--- src/OptionHandlers/PortOptionHandler.cpp
+++ src/OptionHandlers/PortOptionHandler.cpp
@@ -7,36 +7,51 @@
 
 #include "PortOptionHandler.hpp"
 
+#include <iostream>
+
+#include "constants.hpp"
+#include "OptionException.hpp"
+
 namespace ftp {
 bool PortOptionHandler::operator()(
-    const std::vector<std::string> &args, std::vector<std::string> &tempArgs)
+    const std::vector<std::string> &, std::vector<std::string> &tempArgs)
 {
     if (tempArgs.empty())
         return false;
 
-    for (auto itt = args.begin(); itt != args.end(); ++itt) {
-        if (*itt == "-p" || *itt == "--port") {
-            if (itt + 1 == args.end())
-                throw std::invalid_argument("Port option requires an argument");
-            _optionValue = *itt;
-            _hasOption = true;
-            tempArgs.erase(itt + 1);
-            tempArgs.erase(itt);
+    for (auto itt = tempArgs.begin(); itt != tempArgs.end(); ++itt) {
+        if (*itt != "-p" && *itt != "--port")
+            continue;
 
-            return true;
+        try {
+            if (itt + 1 == tempArgs.end())
+                throw std::invalid_argument("");
+            std::stol(*(itt + 1));
+        } catch (const std::exception &err) {
+            throw error::OptionException("-p",
+                "Need a port number (as an integer)");
         }
+
+        _optionValue = *(itt + 1);
+        _hasOption   = true;
+        tempArgs.erase(itt, itt + 2);
+
+        return true;
     }
 
     return false;
 }
+
 std::string PortOptionHandler::getOption() const noexcept
 {
     return _optionValue;
 }
+
 bool PortOptionHandler::hasOption() const noexcept
 {
     return _hasOption;
 }
+
 std::unique_ptr<utils::IOptionHandler> PortOptionHandler::create()
 {
     return std::make_unique<PortOptionHandler>();
