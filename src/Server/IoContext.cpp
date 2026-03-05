344bbbaf1c5d2e16e2f5e790cc43cbda1d73cdcc
janumaruku <janumaruku@users.noreply.github.com>
2026-03-05 06:06:12 +0000

    deploy: 06713d24eab5e9279a87bc182d94ff61e33b5c0e

---
+++ src/Server/IoContext.cpp
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2026
+** myftp
+** File description:
+** IoContext
+*/
+
+#include "IoContext.hpp"
+
+namespace ftp {
+void IOContext::registerAcceptor(const Acceptor &acceptor,
+    AcceptorHandler handler)
+{
+    const int fileDescriptor = acceptor.getSocketFd();
+
+    pollfd pollFd = {
+        .fd = fileDescriptor,
+        .events = POLLIN,
+        .revents = 0,
+    };
+
+    _acceptorFds.push_back(pollFd);
+    _acceptorHandlers[fileDescriptor] = handler;
+}
+
+void IOContext::registerSocket(const Socket &socket, SocketHandler handler)
+{
+    const int fileDescriptor = socket.getFd();
+
+    pollfd pollFd = {
+        .fd = fileDescriptor,
+        .events = POLLIN,
+        .revents = 0,
+    };
+
+    _socketFds.push_back(pollFd);
+    _socketHandlers[fileDescriptor] = handler;
+}
+} // ftp
